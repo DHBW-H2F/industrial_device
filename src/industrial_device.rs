ecd19dda49506ff940eac0f1cce958d83e5050b5
leo <leo.lemaire007@orange.fr>
2024-08-23 15:42:35 +0200

    Boxed error dump registers

--- src/industrial_device.rs
+++ src/industrial_device.rs
@@ -8,5 +8,5 @@ pub trait LocalIndustrialDevice {
     async fn connect(&mut self) -> Result<(), Box<dyn std::error::Error + Send>>;
     async fn dump_registers(
         &mut self,
-    ) -> Result<HashMap<String, Value>, impl std::error::Error + Send>;
+    ) -> Result<HashMap<String, Value>, Box<dyn std::error::Error + Send>>;
 }
