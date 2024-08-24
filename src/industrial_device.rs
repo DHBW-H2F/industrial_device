58bcb3d2cebcb07ede40998ac6c1b3a39de37a10
leo <leo.lemaire007@orange.fr>
2024-08-24 11:11:55 +0200

    Return crate error

--- src/industrial_device.rs
+++ src/industrial_device.rs
@@ -2,12 +2,12 @@ use std::collections::HashMap;
 
 pub mod errors;
 pub mod types;
+
+use errors::IndustrialDeviceError;
 use types::Value;
 
 #[trait_variant::make(IndustrialDevice: Send)]
 pub trait LocalIndustrialDevice {
-    async fn connect(&mut self) -> Result<(), Box<dyn std::error::Error + Send>>;
-    async fn dump_registers(
-        &mut self,
-    ) -> Result<HashMap<String, Value>, Box<dyn std::error::Error + Send>>;
+    async fn connect(&mut self) -> Result<(), IndustrialDeviceError>;
+    async fn dump_registers(&mut self) -> Result<HashMap<String, Value>, IndustrialDeviceError>;
 }
