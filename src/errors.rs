b5c21be35efda2a78505cca034531568db5a54d0
leo <leo.lemaire007@orange.fr>
2024-08-24 12:03:35 +0200

    Added Send trait to err

--- src/errors.rs
+++ src/errors.rs
@@ -1,8 +1,8 @@
 use custom_error::custom_error;
 
 custom_error! {pub IndustrialDeviceError
-    DeviceNotAccessibleError{err: Box<dyn std::error::Error>} = "The device could not be reached : {err:?}",
-    RequestError{err: Box<dyn std::error::Error>} = "There was an error during the request : {err:?}",
-    DeviceNotConnectedError{err: Box<dyn std::error::Error>} = "The device is not connected, you have to ::connect before making requests : {err:?}",
-    ConversionError{err: Box<dyn std::error::Error>} = "There was an error converting data : {err:?}",
+    DeviceNotAccessibleError{err: Box<dyn std::error::Error + Send>} = "The device could not be reached : {err:?}",
+    RequestError{err: Box<dyn std::error::Error + Send>} = "There was an error during the request : {err:?}",
+    DeviceNotConnectedError{err: Box<dyn std::error::Error + Send>} = "The device is not connected, you have to ::connect before making requests : {err:?}",
+    ConversionError{err: Box<dyn std::error::Error + Send>} = "There was an error converting data : {err:?}",
 }
