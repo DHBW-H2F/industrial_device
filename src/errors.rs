97661e41e8bcbaa72b9f5c72c0de9a17d666d02d
leo <leo.lemaire007@orange.fr>
2024-08-24 11:32:44 +0200

    Typo

--- src/errors.rs
+++ src/errors.rs
@@ -3,6 +3,6 @@ use custom_error::custom_error;
 custom_error! {pub IndustrialDeviceError
     DeviceNotAccessibleError{err: Box<dyn std::error::Error>} = "The device could not be reached : {err:?}",
     RequestError{err: Box<dyn std::error::Error>} = "There was an error during the request : {err:?}",
-    DeviceNotConnected{err: Box<dyn std::error::Error>} = "The device is not connected, you have to ::connect before making requests : {err:?}",
+    DeviceNotConnectedError{err: Box<dyn std::error::Error>} = "The device is not connected, you have to ::connect before making requests : {err:?}",
     ConversionError{err: Box<dyn std::error::Error>} = "There was an error converting data : {err:?}",
 }
