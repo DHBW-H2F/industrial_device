e195c6f1177319e35a222f13bca4f9675bedb08b
leo <leo.lemaire007@orange.fr>
2024-08-26 16:58:42 +0200

    Borrowed values

--- src/industrial_device.rs
+++ src/industrial_device.rs
@@ -9,12 +9,11 @@ use types::Value;
 #[trait_variant::make(IndustrialDevice: Send)]
 pub trait LocalIndustrialDevice {
     async fn connect(&mut self) -> Result<(), IndustrialDeviceError>;
-    async fn read_register_by_name(&mut self, name: String)
-        -> Result<Value, IndustrialDeviceError>;
+    async fn read_register_by_name(&mut self, name: &str) -> Result<Value, IndustrialDeviceError>;
     async fn write_register_by_name(
         &mut self,
-        name: String,
-        value: Value,
+        name: &str,
+        value: &Value,
     ) -> Result<(), IndustrialDeviceError>;
     async fn dump_registers(&mut self) -> Result<HashMap<String, Value>, IndustrialDeviceError>;
 }
