22ca8539b4665f1259904c9cfb2dd59e95328095
leo <leo.lemaire007@orange.fr>
2024-08-27 16:15:30 +0200

    Wrong value error

--- src/errors.rs
+++ src/errors.rs
@@ -6,4 +6,5 @@ custom_error! {pub IndustrialDeviceError
     DeviceNotConnectedError{err: Box<dyn std::error::Error + Send>} = "The device is not connected, you have to ::connect before making requests : {err:?}",
     ConversionError{err: Box<dyn std::error::Error + Send>} = "There was an error converting data : {err:?}",
     RegisterNotFoundError{ name: String } = "The register could not be found : {name:?}",
+    WrongValueType{ val: String } = "The Value \"{val}\" can not be converted for this device"
 }
